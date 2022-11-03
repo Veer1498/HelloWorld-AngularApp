403d64c86bb1f6f7fd98083340b7a4550aed1671
Veer1498 <singavarapuveerendra@gmail.com>
2022-11-03 23:25:07 +0530

    [Veer] Added :Ability To Validate User Name with Given Pattern

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imageUrl="../assets/download.jpg";
   url = "https://www.bridgelabz.com";
   userName:string = ""
+  nameError:string = ""
 
 
   ngOnInit(): void{
@@ -20,4 +21,17 @@ export class AppComponent {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
   } 
+
+  onInput($event:any)
+  {
+    console.log("change event occured!",$event.data);
+    const nameRegex=RegExp('[A-Z]{1}[a-z]{2}');
+    if(nameRegex.test(this.userName))
+    {
+      this.nameError="";
+      this.title=this.userName;
+      return;
+    }
+    this.nameError="Name Is Incorrect";
+  }
 }
